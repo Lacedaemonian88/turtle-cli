5322d5f64d46182a7df4aedffa197b660672d2f9
Sapphire Doulas <145626933+Lacedaemonian88@users.noreply.github.com>
2024-05-24 01:43:44 +0000

    updated test

--- tests/test_turt.rb
+++ tests/test_turt.rb
@@ -1,7 +1,15 @@
 require 'minitest/autorun'
-require_relative './turt'
+require_relative '../turt'
 
 class TestTurtle < Minitest::Test
-def setup
-  @Turtle = Turtle.new
+def test_gives_wisdom
+  wisdom = Guidance.new
+  assert wisdom.ask ("words of wisdom")
+  end
+end
+
+class Guidance 
+  def ask wisdom
+    "words of wisdom"
+  end
 end
